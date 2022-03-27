4d4d980b9dc55c095068ded02c6b37bfc00efa84
Ayushi-creator <sharmaayushi62998@gmail.com>
2022-03-28 00:16:13 +0530

    first commit

---
+++ level1_introduction.java
@@ -0,0 +1,78 @@
+// import java.util.*;
+// public class level1_introduction{
+//     //  public static void main(String[] args) {
+//     //      System.out.println("PEPCODERS");
+//         }
+//     }
+// ---------------------------------------------------
+
+// import java.util.*;
+// public class level1_introduction{
+//     public static void main(String[] args) {
+//         int a=10;
+//         int b=20;
+//         int c=30;
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(c);
+//     }
+// }
+
+//=========================================================
+//how to take input from the user
+// import java.util.*;
+// import java.util.Scanner;
+// public class level1_introduction{
+//     public static void main(String[] args) {
+//         Scanner scn=new Scanner(System.in);
+//         int a=scn.nextInt();
+//         int b=scn.nextInt();
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// }
+//--------------------------------------------------------------------
+//loop
+//===========
+//print no. from 1to 5(increasing oder)
+// import java.util.*;
+// public class level1_introduction{
+//     public static void main(String[] args) {
+//         for(int i=1;i<=5;i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+//print no. from 5to 1(decreasing oder)
+// import java.util.*;
+// public class level1_introduction{
+//     public static void main(String[] args) {
+//         for(int i=5;i>=1;i--){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+//tabel of 2
+// import java.util.*;
+// public class level1_introduction{
+//     public static void main(String[] args) {
+//         for(int i=1;i<=5;i++){
+//             System.out.println(2*i);
+//         }
+//     }
+// }
+
+///////////////////////////////////////(OR)////////////////////////////////
+// import java.util.*;
+// public class level1_introduction{
+//     public static void main(String[] args) {
+//         int number=4;
+//         for(int i=1;i<=5;i++){
+//             System.out.println(number+"X"+i+"="+number*i);
+//         }
+//     }
+// }
+
+
